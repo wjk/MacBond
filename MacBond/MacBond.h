ae547c5dfdd51d9710d2f05915bf5f89e72b1883
William Kent <wjk011@gmail.com>
2015-02-07 21:14:05 -0500

    Initial commit

---
+++ MacBond/MacBond.h
@@ -0,0 +1,12 @@
+//
+//  MacBond.h
+//  MacBond
+//
+//  Created by William Kent on 2/7/15.
+//  Copyright (c) 2015 William Kent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+FOUNDATION_EXPORT double MacBondVersionNumber;
+FOUNDATION_EXPORT const unsigned char MacBondVersionString[];
